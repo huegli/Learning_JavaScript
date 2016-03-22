9077eb0d83ed4fcbe377479ed1712ecaca015686
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2016-03-22 13:15:30 -0700

    Update for ch 12 p5

--- Getting_Started_with_p5.js/Chapter12/sketch.js
+++ Getting_Started_with_p5.js/Chapter12/sketch.js
@@ -6,11 +6,11 @@ function setup() {
     // Create the new file
     output = createWriter("botArmy.tsv");
     // Write a header line with the column titles
-    output.println("type\ntx\ty");
+    output.println("type\tx\ty");
     for (var y = 0; y <= height; y += 60) {
         for (var x = 0; x <= width; x += 20) {
             var robotType = int(random(1,4));
-            output.plintln(robotType + "\t" + x + "\t" + y);
+            output.println(robotType + "\t" + x + "\t" + y);
             ellipse(x, y, 12, 12);
         }
     }

271b513b0ef2a14e69124592fdd39b55b69d1930
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2016-03-22 21:01:25 +0000

    Finalized ch12 for p5

--- Getting_Started_with_p5.js/Chapter12/sketch.js
+++ Getting_Started_with_p5.js/Chapter12/sketch.js
@@ -1,19 +1,24 @@
-var output;
+var numRobotTypes = 3;
+var images = [];
+var scaling = 0.15;
+var botArmy;
+
+function preload() {
+    for (var i = 0; i < numRobotTypes; i++) {
+        images[i] = loadImage("robot" + (i+1) + ".svg");
+    }
+    botArmy = loadTable("botArmy.tsv", "tsv", "header");
+}
 
 function setup() {
     createCanvas(720, 480);
+    imageMode(CENTER);
+    for (var i = 0; i < botArmy.getRowCount(); i++) {
+        var robotType = botArmy.getNum(i, "type");
+        var x = botArmy.getNum(i, "x");
+        var y = botArmy.getNum(i, "y");
 
-    // Create the new file
-    output = createWriter("botArmy.tsv");
-    // Write a header line with the column titles
-    output.println("type\tx\ty");
-    for (var y = 0; y <= height; y += 60) {
-        for (var x = 0; x <= width; x += 20) {
-            var robotType = int(random(1,4));
-            output.println(robotType + "\t" + x + "\t" + y);
-            ellipse(x, y, 12, 12);
-        }
+        var bot = images[robotType - 1];
+        image(bot, x, y, bot.width*scaling, bot.height*scaling);
     }
-
-    output.close();
 }

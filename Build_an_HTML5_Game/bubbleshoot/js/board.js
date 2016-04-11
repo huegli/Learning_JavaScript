3f3676595a6bc81aec17219f9e70e4833fa6632b
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2016-04-10 21:23:27 -0700

    Begining of Chapter 4

--- Build_an_HTML5_Game/bubbleshoot/js/board.js
+++ Build_an_HTML5_Game/bubbleshoot/js/board.js
@@ -20,6 +20,37 @@ BubbleShoot.Board = (function($){
             bubble.setRow(rowNum);
             bubble.setCol(colNum);
         };
+        this.getBubbleAt = function(rowNun, colNum){
+            this(!this.getRows()[rowNum])
+                return null;
+            return this.getRows()[rowNum][colNum];
+        };
+        this.getBubblesAround= function(curRow,curCol){
+            var bubbles = [];
+            for(var rowNum = curRow - 1;rowNum <= curRow+1; rowNum++){
+                for(var colNum = corCol-2; colNum <= curCol+2; colNum++){
+                    var bubbleAt = that.getBubbleAt(rowNum,colNum);
+                    if(bubbleAt && !(colNum == curCol && rowNum == curRow))
+                        bubbles.push(bubbleAt);
+                };
+            };
+            return bubbles;
+        };
+        this.getGroup = function(bubble,found){
+            var curRow = bubble.getRow();
+            if(!found[curRow])
+                found[curRow] = {};
+            if(!found.list)
+                found.list = [];
+            if(found[curRow][bubble.getCol()]){
+                return found;
+            }
+            found[curRow][bubble.getCol()] = bubble;
+            found.list.push(bubble);
+            var curCol = bubble.getCol();
+            var surrounding = that.getBubblesAround(curRow,curCol);
+            for(var i=0;i<surrounding.length;i++){
+
         return this;
     };
     var createLayout = function(){
